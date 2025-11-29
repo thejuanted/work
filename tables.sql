e71607b6a0f5bc4bb2e9f2888c2a5fb72a3dea70
thejuanted <48632942+thejuanted@users.noreply.github.com>
2025-11-28 19:29:21 -0600

    Update tables.sql

--- tables.sql
+++ tables.sql
@@ -6,8 +6,19 @@ CREATE TABLE club_member (
     phoneNumber VARCHAR(15) NULL,
     email VARCHAR(60) NULL,
 
-    -- Constraints
 constraint 	ClubMemberPK primary key (userID),
 constraint 	ClubMemberAK unique (phoneNumber),
 constraint userTypeCheck Check (userType in ('Advisor','Officer','Reserver','Member'))
+);
+
+CREATE TABLE reservation (
+    userID INT NOT NULL,
+    bedNO INT NOT NULL,
+    semesterAssigned DATE NOT NULL,
+
+    CONSTRAINT ReservationPK PRIMARY KEY (userID, bedNO),
+    CONSTRAINT ReservationFK FOREIGN KEY (userID) REFERENCES club_member(userID)
+        	ON UPDATE CASCADE ON DELETE RESTRICT,
+    CONSTRAINT FK_BedNo FOREIGN KEY (bedNO) REFERENCES Bed(bedNO)
+            ON UPDATE CASCADE ON DELETE RESTRICT
 );
