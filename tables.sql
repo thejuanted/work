cfb648c066981c84d41bce3fe8e432f752f51a0f
thejuanted <48632942+thejuanted@users.noreply.github.com>
2025-11-28 20:22:37 -0600

    Update tables.sql

--- tables.sql
+++ tables.sql
@@ -17,8 +17,39 @@ CREATE TABLE reservation (
     semesterAssigned DATE NOT NULL,
 
     CONSTRAINT ReservationPK PRIMARY KEY (userID, bedNO),
-    CONSTRAINT ReservationFK FOREIGN KEY (userID) REFERENCES club_member(userID)
+    CONSTRAINT ReservationFK FOREIGN KEY (userID) 
+            REFERENCES club_member(userID)
         	ON UPDATE CASCADE ON DELETE RESTRICT,
     CONSTRAINT FK_BedNo FOREIGN KEY (bedNO) REFERENCES Bed(bedNO)
-            ON UPDATE CASCADE ON DELETE RESTRICT
+            ON UPDATE CASCADE 
+            ON DELETE RESTRICT
 );
+
+Create TABLE garden (
+    gardenID INT NOT NULL AUTO_INCREMENT,
+    gardenName varchar(30) NOT NULL,
+    gardenLocation varchar(30),
+    
+    CONSTRAINT gardenPK PRIMARY KEY (gardenID)
+);
+
+CREATE TABLE Bed (
+    bedNO INT NOT NULL, 
+    gardenID INT NOT NULL, 
+    size INT NOT NULL, 
+    soilType VARCHAR(15) NOT NULL,
+    bedStatus VARCHAR(15) NOT NULL,
+
+    CONSTRAINT bedPK PRIMARY KEY (bedNo, gardenID), 
+    CONSTRAINT bedFK FOREIGN KEY (gardenID) 
+        REFERENCES garden(gardenID)
+        ON UPDATE CASCADE
+        ON DELETE RESTRICT, 
+    CONSTRAINT bedSoilType CHECK (soilType IN ('sand-loamy', 'silt-loamy', 'sandy', 'loamy','clay-loamy','clay')),
+    CONSTRAINT bedStatus CHECK (bedStatus IN ('Planted', 'Turned', 'Cover Crop', 'Reserved', 'Fertilized'))
+
+    ); 
+
+        
+    
+    
