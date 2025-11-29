de850ba1f520d9a91c4fe6cae840928b73e4db03
thejuanted <48632942+thejuanted@users.noreply.github.com>
2025-11-28 19:18:42 -0600

    Update tables.sql

--- tables.sql
+++ tables.sql
@@ -1,8 +1,8 @@
 CREATE TABLE club_member (
     userID INT AUTO_INCREMENT,
     userType VARCHAR(10) NOT NULL,
-    lastName VARCHAR(15) NOT NULL,
-    firstName VARCHAR(15) NOT NULL,
+    lastName VARCHAR(25) NOT NULL,
+    firstName VARCHAR(25) NOT NULL,
     phoneNumber VARCHAR(15) NULL,
     email VARCHAR(60) NULL,
 
